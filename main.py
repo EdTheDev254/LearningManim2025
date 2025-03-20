4d333feba2f70324aaddcc25c87b94458c9bda6a
Edwin Wagha <edwin.wagha20@gmail.com>
2025-03-21 01:06:12 +0300

    Update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from manim import *
 
 class CircleCreator(Scene):
     def construct(self):
-        circle = Circle()
-        circle.set_fill(PINK, opacity=0.5)
+        circle = Circle(radius=1, color=BLUE)
+        #circle.set_fill(PINK, opacity=0.5)
         self.play(Create(circle))
         self.wait(3)
