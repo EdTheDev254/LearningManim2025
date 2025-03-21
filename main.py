e99334ba81cd67fa2ec7c07093a0536392df05e5
Edwin Wagha <edwin.wagha20@gmail.com>
2025-03-22 00:48:01 +0300

    Rotating Objects Basics

--- main.py
+++ main.py
@@ -27,3 +27,28 @@ class TransformToCircle(Scene):
         self.play(Transform(square, new_square))
         self.play(FadeOut(square))
         self.wait(1)
+
+
+class PositioningObjects(Scene):
+    def construct(self):
+        square = Square(color=GREEN)
+        circle = Circle(color=BLUE)
+
+        self.play(Create(square))
+        self.wait(1)
+        circle.next_to(square, RIGHT)
+        self.play(Create(circle))
+        self.wait(1)
+        self.play(circle.animate.move_to(square.get_center()))
+        self.wait(1)
+        self.play(circle.animate.scale(0.2))
+        self.wait(1)
+        self.play(square.animate.rotate_about_origin(PI/4))
+        self.wait(0.5)
+        self.play(square.animate.scale(0.5))
+        self.wait(1)
+        self.play(square.animate.rotate_about_origin(-PI/4))
+        self.wait(1)  
+
+
+
