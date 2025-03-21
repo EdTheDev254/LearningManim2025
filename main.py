8edfea62838370d45960de35bce54df702e32a67
Edwin Wagha <edwin.wagha20@gmail.com>
2025-03-21 22:06:43 +0300

    Square Creating

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ class CircleCreator(Scene):
         #circle.set_fill(PINK, opacity=0.5)
         self.play(Create(circle))
         self.wait(3)
+
+class SquareCreator(Scene):
+    def construct(self):
+        square = Square(color=GREEN)
+        self.play(Create(square))
+        self.wait(3)
