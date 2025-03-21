f95265f9f97113eaa056fed0e4e8a214ca276b42
Edwin Wagha <edwin.wagha20@gmail.com>
2025-03-21 23:32:09 +0300

    Transforming Shapes

--- main.py
+++ main.py
@@ -12,3 +12,18 @@ class SquareCreator(Scene):
         square = Square(color=GREEN)
         self.play(Create(square))
         self.wait(3)
+
+
+class TransformToCircle(Scene):
+    def construct(self):
+        square = Square(color=GREEN)
+        circle = Circle(color=BLUE)
+        new_square = square.copy()
+
+        self.play(Create(square))
+        self.wait(1)
+        self.play(Transform(square, circle))
+        self.wait(1)
+        self.play(Transform(square, new_square))
+        self.play(FadeOut(square))
+        self.wait(1)
